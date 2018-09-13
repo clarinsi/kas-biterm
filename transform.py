abe7ceddd216e1fe054fcc66f37e4928e2e8fdf3
Nikola Ljubešić <nljubesi@gmail.com>
2018-09-13 15:00:48 +0000

    Major updates, for production use

--- transform.py
+++ transform.py
@@ -2,20 +2,39 @@ import sys
 from lxml import etree
 
 def process_element(child,tag='O'):
+  global sid
+  global tid
   if child.tag=='{http://www.tei-c.org/ns/1.0}w':
-    sys.stdout.write((child.text+'\t'+child.get('ana')[4:]+'\t'+child.get('lemma')+'\t'+tag+'\n').encode('utf8'))
+    tid+=1
+    f.write((str(sid+1)+'.'+str(tid)+'\t'+child.text+'\t'+child.get('ana')[4:]+'\t'+child.get('lemma')+'\t'+tag+'\n').encode('utf8'))
   elif child.tag=='{http://www.tei-c.org/ns/1.0}pc':
-    sys.stdout.write((child.text+'\t'+child.get('ana')[4:]+'\t'+child.text+'\t'+tag+'\n').encode('utf8'))
+    tid+=1
+    f.write((str(sid+1)+'.'+str(tid)+'\t'+child.text+'\t'+child.get('ana')[4:]+'\t'+child.text+'\t'+tag+'\n').encode('utf8'))
 
 root=etree.parse(open('kas-biterm.body.ana.xml'))
 lang=None
-for sentence in root.iterfind('.//{http://www.tei-c.org/ns/1.0}s'):
-  for child in sentence:
-    process_element(child)
-    if child.tag=='{http://www.tei-c.org/ns/1.0}term':
-      lang=child.get('{http://www.w3.org/XML/1998/namespace}lang')
-      for grandchild in child:
-        process_element(grandchild,lang.upper())
-      lang=None
-  sys.stdout.write('\n')
-        
+sent=''
+i=0
+for div in root.iterfind('.//{http://www.tei-c.org/ns/1.0}div'):
+  pattern=div.get('{http://www.w3.org/XML/1998/namespace}n')
+  i+=1
+  f=open('kas-biterm.body.ana.'+str(i)+'.txt','w')
+  for sid,sentence in enumerate(div.iterfind('.//{http://www.tei-c.org/ns/1.0}s')):
+    tid=0
+    for child in sentence:
+      process_element(child)
+      if child.tag=='{http://www.tei-c.org/ns/1.0}term':
+        lang=child.get('{http://www.w3.org/XML/1998/namespace}lang')
+        if lang in ('en','und'):
+          lang='for'
+        for grandchild in child:
+          process_element(grandchild,lang.upper()+'-TERM')
+        lang=None
+      elif child.tag=='{http://www.tei-c.org/ns/1.0}abbr':
+        lang=child.get('{http://www.w3.org/XML/1998/namespace}lang')
+        if lang in ('en','und'):
+          lang='for'
+        for grandchild in child:
+          process_element(grandchild,lang.upper()+'-ABBR')
+        lang=None
+    f.write('\n')
